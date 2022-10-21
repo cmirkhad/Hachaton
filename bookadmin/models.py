91d60011e9ae435f18ba377c5776425b986a2322
Mirkhad <cmirkhad@mail.ru>
2022-10-22 01:24:07 +0600

    changed bookadmin models

--- bookadmin/models.py
+++ bookadmin/models.py
@@ -1,17 +1,32 @@
+from datetime import timedelta
+
 from django.db import models
 
 # Create your models here.
-class Books(models.Model):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    image = models.ImageField(upload_to=)
-    publisher = models.CharField(max_length=256)
-    category = models.ForeignKey('Category')
-    author = models.ForeignKey('Author')
-    count = models.PositiveIntegerField()
-    reserved_c = models.PositiveIntegerField()
 
 class Author(models.Model):
     name = models.CharField(max_length=120)
     surname = models.CharField(max_length=120)
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    image = models.ImageField(upload_to='/')
+    publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT, blank=True, null=True)
+    category = models.ManyToManyField(Category)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    count = models.PositiveIntegerField(default=5)
+    reserved_c = models.PositiveIntegerField(default=0)
+    duration = models.DurationField(timedelta(days=7))
+
+
 
