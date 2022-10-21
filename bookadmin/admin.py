d54542fc71d4a8a1bf35cc2aed846e4285796221
Mirkhad <cmirkhad@mail.ru>
2022-10-22 01:58:19 +0600

    requirements fixed

--- bookadmin/admin.py
+++ bookadmin/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from Hachaton.bookadmin.models import Books, Author
+from Hachaton.bookadmin.models import Books, Author, Publisher
 
 admin.site.register(Books)
 admin.site.register(Author)
-
+admin.site.register()
