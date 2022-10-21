b7ec167de706404fe8020e2fdca91fc38e4607b3
Fattijenishbek <fattijenishbek01@gmail.com>
2022-10-21 21:54:13 +0600

    Revert "venv"
    This reverts commit e10e2a6e7b788d556af41d0f29afa0364dfee506.

--- admin/models.py
+++ admin/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-
-class Book(models.Model):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    image = models.ImageField()
+# Create your models here.
