9e0fc1aa92d8ca7b11e7063d96c6eb9ab0f31311
Mirkhad <cmirkhad@mail.ru>
2022-10-22 01:28:18 +0600

    conflict fixed 2

--- bookadmin/models.py
+++ bookadmin/models.py
@@ -2,40 +2,11 @@ from datetime import timedelta
 
 from django.db import models
 
-<<<<<<< HEAD
-
-class Books(models.Model):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    image = models.ImageField(upload_to='images')
-    publisher = models.CharField(max_length=256)
-    # category = models.ForeignKey('Category')
-    # author = models.ForeignKey('Author')
-    count = models.PositiveIntegerField()
-    reserved_c = models.PositiveIntegerField()
-||||||| 276dac3
-# Create your models here.
-class Books(models.Model):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    image = models.ImageField(upload_to=)
-    publisher = models.CharField(max_length=256)
-    category = models.ForeignKey('Category')
-    author = models.ForeignKey('Author')
-    count = models.PositiveIntegerField()
-    reserved_c = models.PositiveIntegerField()
-=======
-# Create your models here.
->>>>>>> mirkhad
-
 
 class Author(models.Model):
     name = models.CharField(max_length=120)
     surname = models.CharField(max_length=120)
-<<<<<<< HEAD
-||||||| 276dac3
 
-=======
 
 class Publisher(models.Model):
     name = models.CharField(max_length=256)
@@ -43,7 +14,6 @@ class Publisher(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
-
 
 
 class Books(models.Model):
@@ -58,5 +28,3 @@ class Books(models.Model):
     duration = models.DurationField(timedelta(days=7))
 
 
-
->>>>>>> mirkhad
